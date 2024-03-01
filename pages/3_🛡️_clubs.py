119902059f550a6206121184a11126ae6fe6cff5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-01 16:00:38 -0300

    feat: add clubs stats page

---
+++ pages/3_🛡️_clubs.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title='Clubs',
+  page_icon='🛡️',
+  layout='wide'
+)
+
+df_data = st.session_state['data']
+
+clubs = df_data['Club'].unique()
+club = st.sidebar.selectbox('Club', clubs)
+
+df_filtered = df_data[(df_data['Club'] == club)].set_index('Name')
+
+st.image(df_filtered.iloc[0]['Club Logo'])
+st.markdown(f'## {club}')
+
+columns = ['Age', 'Photo', 'Flag', 'Overall', 'Value(£)', 'Wage(£)', 'Joined', 'Height(cm.)', 'Weight(lbs.)', 'Contract Valid Until', 'Release Clause(£)']
+
+st.dataframe(
+  df_filtered[columns],
+  column_config={
+  'Overall': st.column_config.ProgressColumn('Overall', format='%d', min_value=0, max_value=100),
+  'Wage(£)': st.column_config.ProgressColumn('Weekly Wage', format='£%f', min_value=0, max_value=df_filtered['Wage(£)'].max()),
+  'Photo': st.column_config.ImageColumn(),
+  'Flag': st.column_config.ImageColumn('Country')
+})
