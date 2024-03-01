e0e3fbb5449d5c01b991bc9b27fdee14792e1cea
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-01 14:45:25 -0300

    feat: add club and players selectbox

--- pages/2_🏃🏻‍♂️_players.py
+++ pages/2_🏃🏻‍♂️_players.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
 df_data = st.session_state['data']
-df_data
+
+clubs = df_data['Club'].unique()
+club = st.sidebar.selectbox('Club', clubs)
+
+df_players = df_data[(df_data['Club'] == club)]
+players = df_players['Name'].unique()
+player = st.sidebar.selectbox('Player', players)
